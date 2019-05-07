b59fa938c881b019a7e160066d3c5728461ab72e
Roodiger <rudywolsfeld@gmail.com>
2019-05-06 23:09:26 -0700

    Platformer game
    A test platformer game

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const brickBreakClasses = require('./brickbreak/brickbreakclasses');
+const platformerClasses = require('./platformer/platformerclasses');
 
 module.exports.brickbreak = brickBreakClasses;
+module.exports.platformer = platformerClasses;
